dcd7af2ed9daf56d5623a1e86d3886bf222d7bbb
Corbin-O <111911457+Corbin-O@users.noreply.github.com>
2022-08-25 20:13:16 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Corbin Sanders
+ * Date: 2022/08/25
+ *
+ * A simple program to print a name in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+
+    printf("Corbin Sanders\n");
+
+    return 0;
+}
